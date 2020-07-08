81f35a4024423514bf9b9c9bceeca7bfc9197659
Marcelo Mendonca <marcelomendonca84@gmail.com>
2020-07-08 01:33:10 -0500

    Initial project creation.

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ProjectsCards />
+  </div>
+</template>
+
+<script>
+import ProjectsCards from '~/components/ProjectsCards.vue'
+
+export default {
+  components: {
+    ProjectsCards,
+  },
+}
+</script>
+
+<style></style>
